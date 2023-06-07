f4c04d888f676a57231763b253265d32c760d729
Roman Pulyakho <pulromans@gmail.com>
2023-06-07 14:43:11 +0300

    Fix views.py

--- advertisements/views.py
+++ advertisements/views.py
@@ -24,8 +24,6 @@ class AdvertisementViewSet(ModelViewSet):
 
     def get_permissions(self):
         """Получение прав для действий."""
-
         if self.action in ["create", "update", "partial_update", "destroy"]:
-            print('auth')
             return [IsAuthenticated(), IsOwnerOrReadOnly()]
         return []
